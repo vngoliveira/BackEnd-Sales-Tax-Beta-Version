101034a36fdd26431a1333421630320a176a741e
vngoliveira <vivi40195@gmail.com>
2023-03-23 07:39:33 -0300

    Modify unit tests

--- src/test/java/org/sales/GoodsStatusTest.java
+++ src/test/java/org/sales/GoodsStatusTest.java
@@ -1,2 +1,16 @@
-package org.sales;public class GoodsStatusTest {
+package org.sales;
+
+import org.junit.jupiter.api.Test;
+import org.sales.enums.GoodsStatus;
+import org.sales.exceptions.GoodsInvalidOptionExceptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GoodsStatusTest {
+
+    @Test
+    public void testFromValue_ValidOption_ReturnsGoodsStatus() throws GoodsInvalidOptionExceptions {
+        assertEquals(GoodsStatus.IMPORTED, GoodsStatus.fromValue(0));
+        assertEquals(GoodsStatus.NATIONAL, GoodsStatus.fromValue(1));
+    }
 }
